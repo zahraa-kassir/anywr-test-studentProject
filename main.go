8eefafef6db0ed88f149b1b83cf27e01a74339e6
Ali Zaarour <zaarour.a@outlook.com>
2022-10-24 14:10:57 +0300

    feat(main):set route and http methode

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"test-pr/anywr-test-studentProject/controller"
 	"test-pr/anywr-test-studentProject/db"
+	"test-pr/anywr-test-studentProject/repository"
 )
 
 func main() {
@@ -15,6 +17,20 @@ func main() {
 
 	// Connect to the "bank" database
 	db.Connect("postgres://postgres:admin@localhost:5432/anywr-student-project")
+
+	//Routes
+	classController := controller.ClassController{
+		ClassRepository: repository.ClassRepository{
+			DB: db.Instance,
+		},
+	}
+
+	//main group
+	maingrp := e.Group("/anywrUni")
+
+	maingrp.GET("/UniClass", classController.GetAll)
+	maingrp.GET("/UniClass/:id", classController.GetById)
+	maingrp.GET("/UniClass/:code", classController.GetByCode)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
