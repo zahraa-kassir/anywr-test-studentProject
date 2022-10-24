743d43c011a6e229e7bbd984a2f8eaae84d2a0cc
Ali Zaarour <zaarour.a@outlook.com>
2022-10-24 18:09:34 +0300

    feat,fix(class-repo): fix get data methode , add new get by student email

--- repository/class-repository.go
+++ repository/class-repository.go
@@ -17,12 +17,22 @@ func (c ClassRepository) GetAll() []entity.UniClass {
 
 func (c ClassRepository) GetById(id int) entity.UniClass {
 	var class = entity.UniClass{Id: id}
-	_ = c.DB.First(&class)
+	_ = c.DB.Take(&class)
 	return class
 }
 
 func (c ClassRepository) GetByCode(code string) entity.UniClass {
-	var class = entity.UniClass{Code: code}
-	_ = c.DB.First(&class)
+	var class entity.UniClass
+	_ = c.DB.Take(&class, "code = ?", code)
 	return class
+}
+
+func (c ClassRepository) GetDataOfThisStudent(id int) []entity.UniClass {
+	var classes []entity.UniClass
+	if dbc := c.DB.Table("uni_classes").
+		Joins("inner join student_classes sc on uni_classes.id = sc.class_id").
+		Find(&classes, "stud_id = ? ", id); dbc.Error != nil {
+		return nil
+	}
+	return classes
 }
