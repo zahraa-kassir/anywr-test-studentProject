b0a1fad45bb22a05f4656006301becf5f494e991
Ali Zaarour <zaarour.a@outlook.com>
2022-10-26 23:10:31 +0300

    feat(update and new func): update repo func and fix last

--- repository/student_repository.go
+++ repository/student_repository.go
@@ -11,14 +11,37 @@ type StudentRepository struct {
 
 func (s StudentRepository) GetAll() []entity.Student {
 	var students []entity.Student
-	_ = s.DB.Find(&students)
+	_ = s.DB.Preload("Classes").Find(&students)
 	return students
 }
-
 func (s StudentRepository) GetByEmail(email string) *entity.Student {
 	var student entity.Student
-	if dbc := s.DB.Where("email = ?", email).First(&student); dbc.Error != nil {
+	if dbc := s.DB.Preload("Classes").Where("students.email = ?", email).Find(&student); dbc.Error != nil {
 		return nil
 	}
 	return &student
+}
+
+func (s StudentRepository) GetByCode(code int) []entity.Student {
+	var students []entity.Student
+	if dbc := s.DB.Preload("Classes").Find(&students, "class = ?", code); dbc.Error != nil {
+		return nil
+	}
+	return students
+
+}
+
+func (s StudentRepository) GetByFilter(code int) []entity.Student {
+
+	var students []entity.Student
+
+	if dbc := s.DB.
+		Where("students.class = ?", code).
+		Preload("Classes", "id = ? ", code).
+		Find(&students); dbc.Error != nil {
+		return nil
+	}
+
+	return students
+
 }
--- repository/teacher_repository.go
+++ repository/teacher_repository.go
@@ -11,14 +11,34 @@ type TeacherRepository struct {
 
 func (t TeacherRepository) GetAll() []entity.Teacher {
 	var teachers []entity.Teacher
-	_ = t.DB.Find(&teachers)
+	_ = t.DB.Preload("Classes").
+		Find(&teachers)
 	return teachers
 }
 
 func (t TeacherRepository) GetByEmail(email string) *entity.Teacher {
 	var teacher entity.Teacher
-	if dbc := t.DB.Where("email = ?", email).First(&teacher); dbc.Error != nil {
+	if dbc := t.DB.Preload("Classes").First(&teacher, "email = ? ", email); dbc.Error != nil {
 		return nil
 	}
 	return &teacher
+}
+
+func (t TeacherRepository) GetByCode(code int) []*entity.Teacher {
+	var teachers []*entity.Teacher
+	if dbc := t.DB.Preload("Classes", "id = ? ", code).
+		Joins("inner join teacher_classes as tc on tc.teach_id = teachers.id and tc.class_id = ? ", code).
+		Find(&teachers); dbc.Error != nil {
+		return nil
+	}
+
+	return teachers
+
+}
+
+func (t TeacherRepository) VerifieIfExist(code int) bool {
+	if teachers := t.GetByCode(code); len(teachers) != 0 {
+		return true
+	}
+	return false
 }
